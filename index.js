da2fade34edef6dd189c9fbcf56c1a6885986cad
suchothendav <suchothendav@paypal.com>
2016-03-05 10:20:15 -0800

    stringify

--- index.js
+++ index.js
@@ -4,23 +4,23 @@
  *
  **/
 const stringify = (text, { encoding = 'utf-8', response = 'string' } = {}) => {
-    let textBuff;
-    let lenBuff;
-    let endBuff;
-    let result;
+
+    let result = [];
 
     if (!text) {
         return;
     }
 
-    textBuff = new Buffer(text);
-    lenBuff = new Buffer(`${text.length}:`);
-    endBuff = new Buffer(',');
+    result.push(new Buffer(`${text.length}:`));
+    result.push(new Buffer(text));
+    result.push(new Buffer(','));
 
-    result = Buffer.concat(lenBuff, textBuff, endBuff);
+    result = Buffer.concat(result);
 
     if (response === 'string') {
         return result.toString();
     }
     return result;
-}
+};
+
+export { stringify };
--- test/test_stringify.js
+++ test/test_stringify.js
@@ -1,15 +1,14 @@
 'use strict';
 
 import assert from 'assert';
-import netstring from '../index'
+import { stringify } from '../index'
 
 describe('stringify', () => {
     const input = 'hello world!';
     const output = '12:hello world!,';
 
     it('should return netstring', () => {
-
-        assert.strictEqual(netstring.stringify(input), output, 'OK netstring');
+        assert.strictEqual(stringify(input), output, 'OK netstring');
 
     });
 });
