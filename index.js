9859365fc036b46b94e9207a2360bfbdd7864023
suchothendav <suchothendav@paypal.com>
2016-03-07 14:30:41 -0800

    Parse

--- index.js
+++ index.js
@@ -1,37 +1,74 @@
 'use strict';
 
 import path from 'path';
-/**
- *
- *
- **/
+
 const NETSTRING_DELIMITER = ',';
 const NETSTRING_SEPARATOR = ':';
-
-const stringify = (input, { encoding = 'utf-8', response = 'string', delimiter = path.delimiter } = {}) => {
+const NETSTRING_SEPARATOR_CODE = 58;
+/**
+ * string ---> netstring
+ * 'hello world!' ---> '12:hello world!,'
+ *
+ * Array of strings ---> netstring
+ * [ 'hello world!', 'hello world!'] ---> '12:hello world!,12:hello world!,'
+ **/
+const netstringify = (string, { encoding = 'utf-8', response = 'string' } = {}) => {
 
     let result = [];
+    let input = [];
 
     if (!input) {
         return;
     }
 
-    input.split(delimiter).forEach((text) => {
+    if (!Array.isArray(string)) {
+        input.push(string);
+    } else {
+        input = string;
+    }
+
+    input.forEach((text) => {
         let netstring = [];
         netstring.push(new Buffer(`${text.length}${NETSTRING_SEPARATOR}`, encoding));
         netstring.push(new Buffer(text, encoding));
         netstring.push(new Buffer(NETSTRING_DELIMITER, encoding));
         netstring = Buffer.concat(netstring);
-        result.push((response === 'string') ? netstring.toString() : netstring);
+        result.push(netstring);
     });
-
-    return result;
+    //For string result
+    if (result.length > 0 && response === 'string') {
+        result = result.map(netstring => netstring.toString(encoding));
+        return result.join('');
+    }
+    //Return as buffer for all the other types.
+    return Buffer.concat(result);
 };
 
 const parse = (netstring, { encoding = 'utf-8', response = 'string' } = {}) => {
+    let result = [];
     if (!netstring) {
         return;
     }
+    netstring = new Buffer(netstring, encoding);
+
+    for (let i = 0, val, lenStart = 0, len, stringStart, string; i < netstring.length ; i++) {
+
+        val = netstring[i];
+        //First find the ':' - NETSTRING_SEPARATOR character.
+        if (NETSTRING_SEPARATOR_CODE === val) {
+            //Find the length digits on the left side of NETSTRING_SEPARATOR(:)
+            len = netstring.toString(encoding, lenStart, i);
+            len = Number.parseInt(len);
+            //Find the string based on the length value and slice the string.
+            stringStart = i + 1;
+            string = netstring.slice(stringStart, stringStart + len);
+            result.push((response === 'string') ? string.toString(encoding): string);
+            //Reset index to next string.
+            i = stringStart + len + 1;
+            lenStart = i;
+        }
+    }
+    return result;
 };
 
-export { stringify };
+export { netstringify, parse };
--- test/test_stringify.js
+++ test/test_stringify.js
@@ -1,35 +1,82 @@
 'use strict';
 
 import assert from 'assert';
-import { stringify } from '../index'
+import { netstringify, parse } from '../index'
 import path from 'path';
 
 describe('stringify', () => {
     const input = 'hello world!';
     const expected = '12:hello world!,';
     var result;
+    var inputs = [];
+    var expecteds = [];
+
+    for (var i = 0; i < 3; i++) {
+        inputs.push(input);
+        expecteds.push(expected);
+    }
 
     it('should return netstring', () => {
-        result = stringify(input);
-        assert.ok(result && result.length > 0, 'OK result');
-        result.forEach((value) => {
-            assert.strictEqual(value, expected, 'OK netstring');
-        })
+        result = netstringify(input);
+        assert.strictEqual(result, expected, 'OK netstring');
+    });
+
+    it('should return netstring buffer', () => {
+        result = netstringify(input, {
+            response : 'buffer'
+        });
+        assert.ok(Buffer.compare(result, new Buffer(expected)) === 0, 'OK netstring buffer');
     });
 
     it('should return netstrings', () => {
-        var inputs = [];
-        inputs.push(input);
-        inputs.push(input);
-        inputs.push(input);
+        result = netstringify(inputs);
+        assert.strictEqual(result, expecteds.join(''), 'OK netstring');
+    });
 
-        inputs = inputs.join(path.delimiter);
-        result = stringify(inputs);
+    it('should return netstrings buffer', () => {
+
+        var expectedBuff = expecteds.map(netstring => new Buffer(netstring));
+        expectedBuff = Buffer.concat(expectedBuff);
+
+        result = netstringify(inputs, {
+            response : 'buffer'
+        });
+        assert.ok(Buffer.compare(result, expectedBuff) === 0, 'OK netstrings buffer');
+    });
+
+    it('should parse netstring', () => {
+        result = parse(netstringify(input));
         assert.ok(result && result.length > 0, 'OK result');
-
         result.forEach((value) => {
-            assert.strictEqual(value, expected, 'OK netstring');
-        })
+             assert.strictEqual(value, input, 'OK parsed value');
+        });
+    });
 
+    it('should parse netstring array', () => {
+        result = parse(netstringify(inputs));
+        assert.ok(result && result.length > 0, 'OK result');
+        result.forEach((value) => {
+             assert.strictEqual(value, input, 'OK parsed value');
+        });
+    });
+
+    it('should parse netstring to buffer result', () => {
+        result = parse(netstringify(input), {
+            response : 'buffer'
+        });
+        assert.ok(result && result.length > 0, 'OK result');
+        result.forEach((value) => {
+             assert.ok(Buffer.compare(value, new Buffer(input)) === 0, 'OK netstrings buffer');
+        });
+    });
+
+    it('should parse netstring array to buffer result', () => {
+        result = parse(netstringify(inputs), {
+            response : 'buffer'
+        });
+        assert.ok(result && result.length > 0, 'OK result');
+        result.forEach((value) => {
+             assert.ok(Buffer.compare(value, new Buffer(input)) === 0, 'OK netstrings buffer');
+        });
     });
 });
